84a61c8588d2f1937f4595993d6c6ebf3b517337
davood <daak1365@yahoo.com>
2017-08-09 00:01:56 +0430

    create client and merg server and client

--- src/main/java/ir/daak1365/daeasysocket/network/Protocol.java
+++ src/main/java/ir/daak1365/daeasysocket/network/Protocol.java
@@ -25,27 +25,6 @@ public abstract class Protocol implements Runnable {
         connectionMade();
     }
 
-//    public Socket getSocket() {
-//        return socket;
-//    }
-
-//    public void setSocket(Socket socket) throws IOException {
-//        this.socket = socket;
-//        this.dataOutput = new DAOutputStream(this.socket.getOutputStream());
-//
-//        connectionMade();
-//    }
-
-//    public void dataReceived(String data) throws IOException{
-//
-//    }
-//
-//    public void dataReceived(byte[] data) throws IOException{
-//
-//    }
-
-//    protected abstract void dataReceived(DAInputStream dataInput) throws IOException;
-
     protected abstract void dataReceived(DAdata dataInput) throws IOException;
 
     protected abstract void connectionMade();
@@ -54,10 +33,17 @@ public abstract class Protocol implements Runnable {
 
 
     public void run() {
-        if ((client != null) && (client.isOpen())) {
             ByteBuffer buffer = null;
-
             while (true) {
+                if ((client == null) || (!client.isOpen())) {
+                    try {
+                        client.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    return;
+                }
+
                 buffer = ByteBuffer.allocate(32);
                 Future readResult = client.read(buffer);
 
@@ -76,67 +62,6 @@ public abstract class Protocol implements Runnable {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
             }
-        }
-
-//        try {
-//            System.out.println(in.readUTF());
-//            out.writeUTF("Thank you for connecting to " + clientServerSocket.getLocalSocketAddress());
-//            //server.close();
-//
-//        }catch(SocketTimeoutException s) {
-//            System.out.println("Socket timed out!");
-//            break;
-//        }catch(IOException e) {
-//            e.printStackTrace();
-//            break;
-//        }
-
-//        try {
-//
-//            while (true){
-//
-//                DataInputStream  dataInput = new DataInputStream(socket.getInputStream());
-
-//                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-    //                if(inputStream.read() <= -1){
-    //                    connectionLost();
-    //                    break;
-    //                }
-
-               // DAInputStream daInputStream = new DAInputStream(dataInput);
-                //byte[] data2 = new byte[in.available()];
-                //in.readLine();
-//                if (dataInput.available() <= 0){
-//                    //try {
-//                        synchronized (this){
-//                            //this.wait();
-//                            //Thread.sleep(1000);
-//                            Thread.yield();
-//                        }
-//
-////                    } catch (InterruptedException e) {
-////                        e.printStackTrace();
-////                    }
-//                }
-//                else{
-//                    byte[] data = new byte[dataInput.available()];
-//                    //dataInput.read(data);
-//                    dataInput.readFully(data);
-//                    // String stringData = new String(data);
-//
-//                    dataReceived(new DAInputStream(new ByteArrayInputStream(data)));
-////                }
-//            }
-//
-//            }catch(SocketTimeoutException s) {
-//                connectionTimeout();
-//                s.printStackTrace();
-//            } catch (IOException e) {
-//                connectionLost();
-//                e.printStackTrace();
-//            }
     }
 }
